8e42584eda61b6456553c2ddf1ccf2909c74afaf
bdbrwr <bart@bdbrwr.nl>
2025-04-28 12:09:30 +0200

    Created unittest setups for textnode, fixed __eq__ and TextType in textnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,19 +1,25 @@
 from enum import Enum
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+class TextType(Enum):
+    NORMAL = "Normal text"
+    BOLD = "**Bold text**"
+    ITALIC = "_Italic text_"
+    CODE = "`Code text`"
+    LINKS = "[anchor text](url)"
+    IMAGE = "![alt text](url)"
 
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
     
-    def __eg__(other):
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+    def __eq__(self, other):
+        return (
+            self.text == other.text and 
+            self.text_type == other.text_type and 
+            self.url == other.url
+        )
     
-    def __repr__():
+    def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
