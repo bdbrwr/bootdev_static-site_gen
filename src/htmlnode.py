3d4c4c807c916bd3d7e1fc77b542a72d5431717a
bdbrwr <bart@bdbrwr.nl>
2025-04-28 13:09:38 +0200

    Created LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,3 +14,19 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag=}, {self.value=}, {self.children=}, {self.props=})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props):
+        super().__init__(tag, value, props=props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Value cannot be None, all leafs must have a value")
+        
+        html = self.value
+
+        if self.tag:
+            html = f"<{self.tag}{self.props_to_html()}>{html}</{self.tag}>"
+        
+        return html
