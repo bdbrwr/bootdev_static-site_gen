b1323619d74af4c3e256bbbe129dcab16589e5ba
bdbrwr <bart@bdbrwr.nl>
2025-04-28 21:00:22 +0200

    Fixed typo super. -> super().__init__

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -35,7 +35,7 @@ class LeafNode(HTMLNode):
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        super.__init__(tag, children=children, props=props)
+        super().__init__(tag, children=children, props=props)
 
     def to_html(self):
         if not self.tag:
