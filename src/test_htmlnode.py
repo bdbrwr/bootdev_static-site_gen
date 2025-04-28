4d398d1ca69a1af33c66570f645dde629696ca07
bdbrwr <bart@bdbrwr.nl>
2025-04-28 20:58:14 +0200

    ParentNode testcases from boot.dev

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,4 +1,4 @@
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 import unittest
 
 class TestHTMLNode(unittest.TestCase):
@@ -41,6 +41,20 @@ class TestLeafNode(unittest.TestCase):
         expected = ('<a href="https://www.bootdev.com">Best programming course</a>')
         self.assertEqual(node.to_html(), expected)
 
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
 
 
 if __name__ == "__main__":
